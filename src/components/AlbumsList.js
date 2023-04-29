78798134302dccd78b4410e9b6025ff2351962f5
unknown <ellaithy80@gmail.com>
2023-04-29 21:43:08 +0300

    Async with RTKQ

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,32 @@
+import { useFetchAlbumsQuery, useAddAlbumsMutation } from "../store";
+import Skeleton from "../components/Skeleton";
+import Button from "../components/Button";
+import AlbumsListItem from "./AlbumsListItem";
+
 function AlbumsList({user}){
+    const {data, error, isFetching} = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumsMutation();
+    let content; 
+
+    const handleClick = () => addAlbum(user);
+
+    if (isFetching) {
+        content = <Skeleton className="h-10 w-full" times={3} />
+    } else if (error) {
+        content = <div>Error loading albums.</div>;
+    } else {
+        content = data.map(album => <AlbumsListItem key={album.id} album={album} />);
+}
+
+
     return(
-        <div>Albums List - {user.name}</div>
+        <div>
+            <div className="m-2 flex flex-row item-center justify-between">
+                <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+                <Button loading={results.isLoading} onClick={handleClick}>+ Add Album</Button>
+            </div>
+            <div>{content}</div>
+        </div>
     );
 }
 
